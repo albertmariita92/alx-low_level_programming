45ca413323ce083f40ecaf33e322eed23dcfc2e2
albertmariita92 <experthelp92@gmail.com>
2023-02-21 16:13:27 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alpphabet
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'g'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
